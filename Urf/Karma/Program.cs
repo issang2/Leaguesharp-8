1c7fec221275203b57490051ed85fe12306803e3
HyunMi027 <ketsi027@gmail.com>
2015-04-01 09:16:24 +0200

    karma added

---
+++ Urf/Karma/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Karma
+{
+    static class Program
+    {
+        private static readonly Obj_AI_Hero Player = ObjectManager.Player;
+        private static Spell _shield;
+        static void Main(string[] args)
+        {
+            if (Game.Mode == GameMode.Running)
+            {
+                Game_OnGameStart(new EventArgs());
+            }
+
+            Game.OnStart += Game_OnGameStart;
+        }
+
+        private static void Game_OnGameStart(EventArgs eventArgs)
+        {
+            if (Player.ChampionName != "Karma") return;
+
+            Game.PrintChat("Urferino janna by HyunMi loaded");
+            _shield = new Spell(SpellSlot.E, 800);
+
+            Game.OnUpdate += Game_OnUpdate;
+
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (_shield.IsReady())
+            {
+                _shield.CastOnUnit(Player.GetAlliesInRange(800).LastOrDefault());
+            }
+        }
+    }
+}
