a491b86e1cc3e4c9173aa39dbd363a36bb5c76f4
HyunMi027 <ketsi027@gmail.com>
2015-04-01 11:16:39 +0200

    karthus added

---
+++ Urf/Karthus/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace Karthus
+{
+    static class Program
+    {
+
+        private static readonly Obj_AI_Hero Player = ObjectManager.Player;
+        private static Spell _q;
+        static void Main(string[] args)
+        {
+            if (Game.Mode == GameMode.Running)
+            {
+                Game_OnGameStart(new EventArgs());
+            }
+
+            Game.OnStart += Game_OnGameStart;
+        }
+
+        private static void Game_OnGameStart(EventArgs args)
+        {
+
+            if (Player.ChampionName != "Karthus") return;
+            Game.PrintChat("Urferino Karthus by HyunMi loaded");
+            _q = new Spell(SpellSlot.Q, 875);
+            Game.OnUpdate += Game_OnUpdate;
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            if (Player.CountEnemiesInRange(875) > 0 && _q.IsReady())
+            {
+                _q.CastOnUnit(Player.GetEnemiesInRange(875).OrderByDescending(target => target.Health).Last());
+            }
+        }
+    }
+}
